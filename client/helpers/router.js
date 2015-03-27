36793178bedfbd0b0c5cc1b82ebb050c72dfdb2e
Nishad Agrawal <nishadagrawal7@gmail.com>
2015-03-27 17:07:13 -0400

    minor routing updates

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,8 +1,29 @@
 Router.configure({
-    layoutTemplate: 'layout'
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
 });
+
 Router.map(function() {
     this.route('availability', {
+        path: '/availability'
+    });
+
+    this.route('welcome', {
         path: '/'
     });
 });
+
+var requireLogin = function() {
+    if (! Meteor.user()) {
+		if (Meteor.loggingIn()) {
+			this.render(this.loadingTemplate);
+		} else {
+			this.render('accessDenied');			
+		}					
+	} else {
+		this.next(); 
+	}
+}
+Router.before(requireLogin, {
+    only: 'availability'
+})
