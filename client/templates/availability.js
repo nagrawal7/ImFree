7ab3f614762f10c87a50b38c2325c62c2196b063
Nishad Agrawal <nishadagrawal7@gmail.com>
2015-05-14 13:26:34 -0500

    condense avail slots to 1 string

--- client/templates/availability.js
+++ client/templates/availability.js
@@ -9,7 +9,7 @@ Template.availability.events({
         time.second(0);
         for (var i = 0; i < array.length; i++) {
             array[i] = time.format();
-            time.minute(time.minute() + duration);            
+            time.minute(time.minute() + duration);
         }
         Session.set('rows', array);
     },
@@ -18,8 +18,8 @@ Template.availability.events({
         $('#gridcontainer').show();
         var $input = $('.datepicker').pickadate();
         var picker = $input.pickadate('picker');
-        
-        var selectedDate = moment(picker.get('select','mm/dd/yyyy'),'MM-DD-YYYY');
+
+        var selectedDate = moment(picker.get('select', 'mm/dd/yyyy'), 'MM-DD-YYYY');
         week = new Array(7);
         for (var i = 0; i < 7; i++) {
             week[i] = selectedDate.weekday(i).format();
@@ -40,17 +40,17 @@ Template.availability.rendered = function() {
         },
         unselecting: function(event, ui) {
             var location = determineLocation(ui.unselecting);
-            updateAvailable(location, true);            
+            updateAvailable(location, true);
         }
-    });    
+    });
 
     $('#gridcontainer').hide();
 
     $('.dropdown-button').dropdown({
-            constrain_width: true, // Does not change width of dropdown to that of the activator
-            hover: false, // Activate on click
-            alignment: 'center', // Aligns dropdown to left or right edge (works with constrain_width)
-            belowOrigin: true // Displays dropdown below the button
+        constrain_width: true, // Does not change width of dropdown to that of the activator
+        hover: false, // Activate on click
+        alignment: 'center', // Aligns dropdown to left or right edge (works with constrain_width)
+        belowOrigin: true // Displays dropdown below the button
     });
 
     $('select').material_select();
@@ -58,21 +58,22 @@ Template.availability.rendered = function() {
     $('.datepicker').pickadate({
         selectYears: 1 // Creates a dropdown of 15 years to control year
     });
-    
-    if (! Availability.findOne()) {
-        Availability.insert({user: Meteor.userId()});
+
+    if (!Availability.findOne()) {
+        Availability.insert({
+            user: Meteor.userId()
+        });
     }
     // console.log(Availability.findOne());
 }
 
 function determineLocation(cell) {
-    var myDay = Session.get('week')[cell.cellIndex];
+    var myDay = moment(Session.get('week')[cell.cellIndex-1]);
     var index = $(cell).parent().parent().children().index($(cell).parent());
-    var myRow = Session.get('rows')[index];    
-    var obj = {
-        day: myDay,
-        row: myRow
-    };
+    var myRow = moment(Session.get('rows')[index]);
+    myDay.hour(myRow.hour());
+    myDay.minute(myRow.minute());
+    var obj = myDay.format();
     console.log(obj);
     return obj;
 }
@@ -81,7 +82,7 @@ function updateAvailable(obj, toDelete) {
     var array = Session.get('available');
     var found = false;
     for (var i = 0; i < array.length; i++) {
-        if (array[i].day === obj.day && array[i].row === obj.row) {
+        if (array[i] === obj) {
             if (toDelete) {
                 array.splice(i, 1);
                 Session.set('available', array);
